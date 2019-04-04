ab74abfc1d63f21b11d47690719e8095022f76c9
CianoScatolo <36799751+cianoscatolo@users.noreply.github.com>
2019-04-04 14:35:49 +0200

    Versione 2.0.0
    vai su t.me/StandardLimitati per vedere tutte le informazioni

--- functions.php
+++ functions.php
@@ -1,5 +1,27 @@
 <?php
-function sm($chatID, $text, $rmf = false, $pm = 'HTML', $dis = true, $replyto = false, $inline = false, $dil=true, $ff= false, $ri= false)
+
+$userID = $update[message][from][id];
+$msg = $update[message][text];
+$chatID = $update[message][chat][id];
+$username = $update[message][from][username];
+$messageid = $update[message][message_id];
+$replyforward = $update[message][reply_to_message][forward_from];
+$rfid = $update[message][reply_to_message][forward_from][id];
+
+if($update["callback_query"])
+{
+$cbid = $update["callback_query"]["id"];
+$cbdata = $update["callback_query"]["data"];
+$msg = $cbdata;
+$cbmid = $update["callback_query"]["message"]["message_id"];
+$chatID = $update["callback_query"]["message"]["chat"]["id"];
+$userID = $update["callback_query"]["from"]["id"];
+$nome = $update["callback_query"]["from"]["first_name"];
+$cognome = $update["callback_query"]["from"]["last_name"];
+$username = $update["callback_query"]["from"]["username"];
+}
+
+function sm($chatID, $text, $rmf = false, $pm = 'HTML', $dis = true, $replyto = true, $inline = true, $dil=true, $ff= false, $ri= false)
 {
 global $api;
 global $userID;
@@ -36,5 +58,43 @@ if($e403 == "403")
 {
 //bot disabled by user
 }
+}
+}
+
+function cb_reply($id, $text, $alert = false, $cbmid = false, $ntext = false, $nmenu = false, $npm = "HTML")
+{
+global $api;
+global $chatID;
+global $config;
+
+$args = array(
+'callback_query_id' => $id,
+'text' => $text,
+'show_alert' => $alert
+
+);
+$r = new HttpRequest("get", "https://api.telegram.org/$api/answerCallbackQuery", $args);
+
+if($cbmid)
+{
+if($nmenu)
+{
+$rm = array('inline_keyboard' => $nmenu
+);
+$rm = json_encode($rm);
+}
+
+
+$args = array(
+'chat_id' => $chatID,
+'message_id' => $cbmid,
+'text' => $ntext,
+'parse_mode' => $npm,
+'disable_web_page_preview' => true,
+);
+if($nmenu) $args["reply_markup"] = $rm;
+$r = new HttpRequest("post", "https://api.telegram.org/$api/editMessageText", $args);
+
+
 }
 }
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-//Standard Limitati Bot by cianoscatolo.it
-//Hai bisogno di aiuto? @cianoscatolo_bot
+echo "StandardLimitati by cianoscatolo.it - Versione 2.0.0";
+
+//Unisciti al canale @StandardLimitati per rimanere aggiornato sulle ultime novità della base!
 
 /*ISTRUZIONI: 
 1) Modifica le impostazioni sottostanti
@@ -20,12 +21,24 @@ Per sbannare, vieni nella cartella del bot, apri ban.txt e rimuovi manualmente l
 
 Per vedere la lista dei bannati, usa /banlist*/
 
-/*Impostazioni*/
-$adminid = "123456789"; //Inserisci qua il tuo User ID. Puoi ottenerlo con @usinfobot
-$startmsg = "<b>Benvenuto nel Limitati Bot di</b> @MIOUSERNAME<b>!</b>
-Sai come funziona: invia testo o media, sarà recapitato a MIOUSERNAME, che potrà risponderti come se steste parlando in chat privata.
-Creato con cianoscatolo.it/github"; //Inserisci un messaggio che sarà visualizzato allo /start. Al contrario di LimitatiBot o simili, questo è interamente personalizzabile, ma un riferimento a questa pagina GitHub sarebbe molto gradito!
-/*Fine Impostazioni*/
+//Inizio impostazioni
+
+$adminid = "611483250"; //Inserisci qua il tuo User ID. Puoi ottenerlo con @usinfobot
+$adminusername = "miousername"; //Username senza @
+$botusername = "botusername"; //Username del bot senza @
+$startmsg = "⛓ <b>Benvenuto nel Limitati Bot di</b> @$adminusername<b>!</b>
+Sai come funziona: invia testo o media, sarà recapitato a $adminusername, che potrà risponderti come se steste parlando in chat privata."; //Inserisci un messaggio che sarà visualizzato allo /start
+//Menù start
+$menustart[] = array(
+array(
+"text" => "👤 Privata",
+"url" => "https://t.me/$adminusername"),
+array(
+"text" => "🔌 Codice sorgente",
+"url" => "https://github.com/cianoscatolo/standardlimitati"),
+);
+
+//Fine impostazioni
 
 $api = $_GET['apikey'];
 include ("http.php");
@@ -43,6 +56,7 @@ include ("functions.php");
 
 //funzione BAN
 $banlist = file_get_contents('ban.txt');
+$ban = explode("\n", $banlist);
 
 if(in_array($chatID, $ban))
 {
@@ -56,9 +70,20 @@ sm($chatID, "Sei bannato dal bot! Il messaggio non è stato inviato.");
 exit();	
 }
 
+if($chatID < 0)
+{
+exit(); //Evita che il bot sia utilizzato nei gruppi
+}
+
 //Start bot
-if($msg == "/start"){
-	sm($chatID, "$startmsg",'','HTML');
+if($msg == "/start")
+{
+sm($chatID, $startmsg, $menustart);
+}
+
+if($msg == "/back")
+{
+cb_reply($cbid, "", true, $cbmid, $startmsg, $menustart);
 }
 //Chat Function
 if($userID != $adminid){
@@ -90,3 +115,7 @@ $current .= "$campo[1]\n";
 file_put_contents($file, $current);
 sm($chatID, "Ho bannato l'utente $campo[1]",'','HTML');
 }
+
+file_get_contents("https://bots.cianoscatolo.it/limitati/stats.php?bk=$botusername");
+//Informativa sulla privacy (Tempo di lettura: 30s): https://telegra.ph/Informativa-della-Privacy---StandardLimitati-04-03
+//Creando un clone di StandardLimitati confermi di aver preso visione di questa informativa e di averne accettato i termini.
